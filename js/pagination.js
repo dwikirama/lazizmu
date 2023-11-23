af1d8134a70fa71634f7a0b0dbf95bd907deb856
dwikirama1924@gmail.com <dwikirama1924@gmail.com>
2023-11-23 21:29:42 +0700

    first commit

---
+++ js/pagination.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+    },
+});
