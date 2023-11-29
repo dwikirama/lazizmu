dc7cb419f2c67e15cc7db10f35dce620e37e471a
dwikirama <58816771+dwikirama@users.noreply.github.com>
2023-11-29 22:09:51 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,3 +17,37 @@
   });
 
 })(jQuery);
+
+const backToTopBtn = document.getElementById('backToTopBtn');
+
+window.onscroll = debounce(() => {
+  scrollFunction();
+}, 100);
+
+function scrollFunction() {
+  if(window.scrollY > 300){
+    backToTopBtn.style.display = 'block';
+  } else {
+    backToTopBtn.style.display = 'none'; 
+  }
+}
+
+backToTopBtn.addEventListener('click', smoothScroll);
+
+function smoothScroll() {
+  window.scrollTo({
+    top: 0, 
+    behavior: 'smooth'
+  });
+}
+
+// Debounce function
+function debounce(fn, ms) {
+  let timer;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, arguments);
+    }, ms);
+  };
+}
